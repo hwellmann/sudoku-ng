8fd657a912994c96b295144e6a2fb15d5cfc2dda
Harald Wellmann <harald.wellmann@gmx.de>
2019-08-10 17:02:21 +0200

    use @jest/console

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -3,28 +3,28 @@ import 'jest-preset-angular';
 
 /* global mocks for jsdom */
 const mock = () => {
-  let storage: { [key: string]: string } = {};
-  return {
-    getItem: (key: string) => (key in storage ? storage[key] : null),
-    setItem: (key: string, value: string) => (storage[key] = value || ''),
-    removeItem: (key: string) => delete storage[key],
-    clear: () => (storage = {})
-  };
+    let storage: { [key: string]: string } = {};
+    return {
+        getItem: (key: string) => (key in storage ? storage[key] : null),
+        setItem: (key: string, value: string) => (storage[key] = value || ''),
+        removeItem: (key: string) => delete storage[key],
+        clear: () => (storage = {})
+    };
 };
 
 Object.defineProperty(window, 'localStorage', { value: mock() });
 Object.defineProperty(window, 'sessionStorage', { value: mock() });
 Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ['-webkit-appearance'],
+    value: () => ['-webkit-appearance'],
 });
 
 Object.defineProperty(document.body.style, 'transform', {
-  value: () => {
-    return {
-      enumerable: true,
-      configurable: true,
-    };
-  },
+    value: () => {
+        return {
+            enumerable: true,
+            configurable: true,
+        };
+    },
 });
 
 
@@ -32,8 +32,19 @@ Object.defineProperty(document.body.style, 'transform', {
 /* output shorter and more meaningful Zone error stack traces */
 // Error.stackTraceLimit = 2;
 
-const { SimpleConsole } = require('./SimpleConsole');
-global.console = new SimpleConsole(process.stdout, process.stderr);
+import { CustomConsole, LogType, LogMessage } from '@jest/console';
+
+function simpleFormatter(type: LogType, message: LogMessage): string {
+    const TITLE_INDENT = '    ';
+    const CONSOLE_INDENT = TITLE_INDENT + '  ';
+
+    return message
+        .split(/\n/)
+        .map(line => CONSOLE_INDENT + line)
+        .join('\n');
+}
+
+global.console = new CustomConsole(process.stdout, process.stderr, simpleFormatter);
 
 
 configure({
