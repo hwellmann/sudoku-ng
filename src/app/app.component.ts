e7401e2de7dc36f80e32b012966a647ed427a8df
Harald Wellmann <harald.wellmann@gmx.de>
2019-08-10 13:40:32 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sudoku-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sudoku-ng';
+}
