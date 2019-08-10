67d5a5e389f06e3f593af0219df1368b54b5a4ad
Harald Wellmann <harald.wellmann@gmx.de>
2019-08-10 22:27:25 +0200

    fixed logging

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { configure, LogLevel } from '@log4js2/core';
+
+import 'hammerjs';
+
+configure({
+    level: LogLevel.INFO,
+    virtualConsole: false
+});
 
 if (environment.production) {
   enableProdMode();
