feacd78bfcdce3a59e0697d3d27338047993bc3e
Harald Wellmann <harald.wellmann@gmx.de>
2019-08-18 10:27:09 +0200

    listen to keyboard events

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Sudoku } from 'app/generator/sudoku';
 import { Cell } from 'app/generator/cell';
+import { Logger, getLogger } from '@log4js2/core';
 
 export interface FieldCssClass {
     field: boolean;
@@ -27,6 +28,19 @@ export abstract class GridApp {
 })
 export class GridComponent {
 
-     constructor(public app: GridApp) { }
+    @ViewChild('grid', { static: true })
+    tableRef: ElementRef;
 
+    private readonly log: Logger = getLogger('GridComponent');
+
+    constructor(public app: GridApp) { }
+
+    onKeyup(event: KeyboardEvent, row: number, col: number) {
+        this.log.debug(`key ${event.key} in (${row}, ${col})`, event.key, row, col);
+        this.tableRef.nativeElement.focus();
+    }
+
+    onTableKeyup(event: KeyboardEvent) {
+        this.log.debug(`key ${event.key}`);
+    }
 }
