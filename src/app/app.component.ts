28bf66c02156e9abd4b49d77e4e0a965a7054a88
Harald Wellmann <harald.wellmann@northdata.de>
2020-07-05 10:24:51 +0200

    use ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { GameController } from './game.controller';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'sudoku-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'sudoku-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnDestroy {
-  title = 'sudoku-ng';
+    title = 'sudoku-ng';
 
-  @ViewChild(MatSidenav, { static: true }) readonly sidenav!: MatSidenav;
+    @ViewChild(MatSidenav, { static: true }) readonly sidenav!: MatSidenav;
 
-  constructor(private gameController: GameController) {
-  }
+    constructor(private gameController: GameController, private translate: TranslateService) {
+        const currentLanguage = this.translate.getBrowserLang();
+        this.translate.use(currentLanguage);
+    }
 
-  ngOnDestroy() {
-      this.gameController.onDestroy();
-  }
-
+    ngOnDestroy() {
+        this.gameController.onDestroy();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,14 +6,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
-import { CandidatesComponent, CandidatesApp } from './candidates/candidates.component';
-import { GridComponent, GridApp } from './grid/grid.component';
-import { SidenavComponent, SidenavApp } from './sidenav/sidenav.component';
-import { ToolbarComponent, ToolbarApp } from './toolbar/toolbar.component';
+import { CandidatesApp, CandidatesComponent } from './candidates/candidates.component';
+import { DigitApp, DigitComponent } from './digit/digit.component';
 import { GameController } from './game.controller';
-import { DigitComponent, DigitApp } from './digit/digit.component';
+import { GridApp, GridComponent } from './grid/grid.component';
+import { SidenavApp, SidenavComponent } from './sidenav/sidenav.component';
+import { ToolbarApp, ToolbarComponent } from './toolbar/toolbar.component';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
 
 @NgModule({
     declarations: [
@@ -28,12 +36,21 @@ import { DigitComponent, DigitApp } from './digit/digit.component';
     imports: [
         BrowserAnimationsModule,
         BrowserModule,
+        HttpClientModule,
         MatButtonModule,
         MatIconModule,
         MatSidenavModule,
         MatSnackBarModule,
         MatToolbarModule,
-        MatTooltipModule
+        MatTooltipModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [ HttpClient ]
+            }
+        })
     ],
     providers: [
         GameController,
